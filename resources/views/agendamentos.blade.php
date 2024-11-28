ec9573767aae96febaeec22bfc3f836a6982861a
Rafael <rafaelteixeiravl@gmail.com>
2024-11-28 20:17:33 -0300

    feat: adiciona imagem de fundo na view agendamento

--- resources/views/agendamentos.blade.php
+++ resources/views/agendamentos.blade.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendamentos</title>
+    <style>
+        body {
+            background-image: url(https://institutoanahickmann.com.br/wp-content/uploads/2021/11/cab.jpg);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+        }
+    </style>
 </head>
 <body>
     <h1>Bem-vindo a tela de Agendamento!</h1>
