1f390397925e42e0ced09fbb9dd5c6e6cae4107d
Rafael Teixeira <133879006+Rafaelvl2023@users.noreply.github.com>
2024-11-28 17:14:39 -0300

    feat: adiciona as migrations

---
+++ database/migrations/2024_11_28_200947_usuarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Executa a criação da tabela de usuários.
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('telefone', 15);
+            $table->string('email')->unique();
+            $table->string('senha');
+            $table->enum('status', ['admin', 'cliente']);
+            $table->date('data_nascimento')->nullable();
+            $table->text('endereco')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverte a criação da tabela de usuários.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
