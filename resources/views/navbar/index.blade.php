5712d58e5c9f28fc28b5e879d3731de91035f3f9
Rafael Teixeira <133879006+Rafaelvl2023@users.noreply.github.com>
2024-12-01 19:51:43 -0300

    feat: adiciona style na nav bar

--- resources/views/navbar/index.blade.php
+++ resources/views/navbar/index.blade.php
@@ -1,46 +1,103 @@
-<nav class="navbar navbar-expand-custom navbar-mainbg">
-    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse"
-        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <i class="fas fa-bars text-white"></i>
-    </button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Navbar Responsiva</title>
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        /* Navbar styling */
+        .navbar-mainbg {
+            background-color: #2c3e50;
+            padding: 0;
+        }
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('agendamentosDashboard.index') }}'">
-                    <i class="far fa-address-book"></i>Agendamentos
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('dashboard.index') }}'">
-                    <i class="fas fa-tachometer-alt"></i>Dashboard
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('gastos_fixos.index') }}'">
-                    <i class="far fa-clone"></i>Gastos Fixos
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('gastos_variados.index') }}'">
-                    <i class="far fa-gastosVariados-alt"></i>Gastos Variados
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('servicos.index') }}'">
-                    <i class="far fa-chart-bar"></i>Serviços
-                </a>
-            </li>
-            <li class="nav-item ml-auto">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="nav-link"
-                        style="background: none; border: none; color: white; padding: 20px 20px; cursor: pointer;">
-                        <i class="fas fa-times"></i> Sair
-                    </button>
-                </form>
-            </li>
-        </ul>
-    </div>
-</nav>
+        .navbar-nav .nav-item .nav-link {
+            color: white;
+            font-size: 16px;
+            padding: 15px 20px;
+            text-transform: uppercase;
+            transition: 0.3s;
+        }
+
+        .navbar-nav .nav-item .nav-link:hover {
+            background-color: #34495e;
+            color: #f39c12;
+            border-radius: 5px;
+        }
+
+        .navbar-toggler {
+            border: none;
+            outline: none;
+        }
+
+        .navbar-toggler:focus {
+            box-shadow: none;
+        }
+
+        .navbar-toggler i {
+            font-size: 24px;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-mainbg">
+        <!-- Botão de alternância visível apenas em telas pequenas -->
+        <button class="navbar-toggler ml-auto d-md-none" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fas fa-bars text-white"></i>
+        </button>
+
+        <!-- Itens da Navbar -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('agendamentos_dashboard.index') }}'">
+                        <i class="far fa-address-book"></i> Agendamentos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('dashboard.index') }}'">
+                        <i class="fas fa-tachometer-alt"></i> Dashboard
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('gastos_fixos.index') }}'">
+                        <i class="far fa-clone"></i> Gastos Fixos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('gastos_variados.index') }}'">
+                        <i class="far fa-chart-bar"></i> Gastos Variados
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);" onclick="window.location.href = '{{ route('servicos.index') }}'">
+                        <i class="far fa-chart-bar"></i> Serviços
+                    </a>
+                </li>
+                <li class="nav-item ml-auto">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="nav-link"
+                            style="background: none; border: none; color: white; padding: 15px 20px; cursor: pointer;">
+                            <i class="fas fa-times"></i> Sair
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Bootstrap JS, jQuery e Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
