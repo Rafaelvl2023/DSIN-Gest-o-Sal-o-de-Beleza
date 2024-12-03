d5538115f44ed032f33501610335df6f370e498b
Rafael Teixeira <133879006+Rafaelvl2023@users.noreply.github.com>
2024-12-02 23:58:56 -0300

    fix: ajuste de valor de quantidade no controller agendamento

--- app/Http/Controllers/AgendamentoController.php
+++ app/Http/Controllers/AgendamentoController.php
@@ -57,7 +57,7 @@ class AgendamentoController extends Controller
                 ->whereBetween('data_agendamento', [$inicio_semana, $fim_semana])
                 ->get();
 
-            if ($agendamentos_na_mesma_semana->count() > 2) {
+            if ($agendamentos_na_mesma_semana->count() >= 1) {
                 $primeiro_agendamento = Carbon::parse($agendamentos_na_mesma_semana->first()->data_agendamento);
 
                 $responseData = [
